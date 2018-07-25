6774b1d229a89780a7b4b54d9a73423ef6ef6253
DMind44 <davidmindlin4@gmail.com>
2018-07-25 15:13:47 -0700

    Update index.js

--- index.js
+++ index.js
@@ -20,7 +20,7 @@ var api = new ParseServer({
   push: {
   android: {
     apiKey: 'AIzaSyDtP54XwGaqah_x0HQ5VRbfPEYotEZymp8' // The Server API Key of FCM
-  },
+  }
   liveQuery: {
     classNames: ["Attraction", "Trip", "Message"] // List of classes to support for query subscriptions
   }
